8eef1bd9760dd9f34e6d86baaf5d40cd2efb054a
Aashutosh Raj <aashutoshraj521@gmail.com>
2026-07-10 16:50:06 +0530

    zeros, ones, another ways to create tensor are added

--- PyTorch/tensor_creation.py
+++ PyTorch/tensor_creation.py
@@ -1,3 +1,9 @@
+'''When writing PyTorch code, make it a reflex to inspect tensors immediately after creating or loading them:
+print(x.shape)
+print(x.dtype)
+print(x.device)
+Many debugging sessions that could take hours are resolved in seconds by checking these three properties. This habit becomes even more valuable once we start implementing CNNs and reproducing research papers.'''
+
 import torch
 
 # Scalar (0-D)
@@ -19,7 +25,26 @@ tensor3d = torch.tensor([
     [[9,10], [11,12]]
 ])
 
-print(scalar.shape)
-print(vector.shape)
-print(matrix.shape)
-print(tensor3d.shape)
+print("Shape of scalar: ", scalar.shape)
+print("Shape of vector: ", vector.shape)
+print("Shape of matrix: ", matrix.shape)
+print("Shape of tensor: ", tensor3d.shape)
+
+# Many bugs are solved just by inspecting these three properties.
+print("Shape of tensor: ", tensor3d.shape)
+print("Tensor data type: ", tensor3d.dtype)
+print("Tensor device: ", tensor3d.device)
+
+# Different ways to create tensors
+a = torch.tensor([1, 2, 3])
+b = torch.zeros((2, 3))
+c = torch.ones((2, 3))
+d = torch.rand((2, 3))
+e = torch.arange(0, 10)
+
+# Inspect one tensor
+print(d)
+print("Tensor:\n", d)
+print("Shape :", d.shape)
+print("Data Type :", d.dtype)
+print("Device :", d.device)
